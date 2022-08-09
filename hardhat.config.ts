2e367869b8f0bb26af91df709d55deecb8e99293
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-08-09 17:11:00 +0300

    Configure tools and package.json

---
+++ hardhat.config.ts
@@ -0,0 +1,63 @@
+import { HardhatUserConfig } from 'hardhat/types';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@typechain/hardhat';
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+import 'hardhat-contract-sizer';
+import 'hardhat-log-remover';
+import 'hardhat-spdx-license-identifier';
+
+const DEFAULT_BLOCK_GAS_LIMIT = 12450000;
+const TRACK_GAS = process.env.TRACK_GAS === 'true';
+const BLOCK_EXPLORER_KEY = process.env.BLOCK_EXPLORER_KEY || '';
+const HARDHATEVM_CHAINID = 31337;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.15',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+            details: {
+              yul: true,
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      hardfork: 'london',
+      blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
+      gas: DEFAULT_BLOCK_GAS_LIMIT,
+      gasPrice: 8000000000,
+      chainId: HARDHATEVM_CHAINID,
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      accounts: {
+        accountsBalance: '1000000000000000000000000',
+      },
+    },
+  },
+  gasReporter: {
+    enabled: TRACK_GAS,
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: false,
+  },
+  etherscan: {
+    apiKey: BLOCK_EXPLORER_KEY,
+  },
+};
+
+export default config;
