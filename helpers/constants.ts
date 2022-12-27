9c07aa047ace9940ebe8c3aaf939c89f5005a6d2
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-12-27 22:04:42 +0400

    Deploy to Goerli (#29)
    * Deploy to Goerli
    
    * Fix linting errors

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -9,19 +9,20 @@ export const NETWORKS: {
     governor: '0x1867c96601bc5fE24F685d112314B8F3Fe228D5A',
     validatorsRegistry: '0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b',
     oracles: [
-      '0x1837Af8856Bc16CE905f2150983Bdb02C4Ea5Eb2',
-      '0x9DC80964798bB88daBAd6d3B99888d31128fe4d3',
-      '0x82D205083dfe9EaEa6220348DEAeb7530719306e',
-      '0x1a13f28e039B37712Add4aaBb2081b5420342A6f',
-      '0x79d3a31b62C6950a4222D6B140921cBFceCe9Ccf',
-      '0x2019150fDBe73ECfb2069204bf736CB77E09a8C3',
-      '0xd11ba065D69f101c32096B451ef43f2C0Ab8c5AE',
-      '0xEF90982c170e53a7325503bfC9C1a6346eeA36c2',
-      '0xAe65570357DFf91663E1BBDC37814310B33914B7',
-      '0x100441334E2a4F82120E33db9288F93643DA9102',
-      '0xdBb21Da71e093E896D591Aa780b10457240Ec4bB',
+      '0xf1a2f8E2FaE384566Fe10f9a960f52fe4a103737',
+      '0xF1091485531122c2cd0Beb6fD998FBCcCf42b38C',
+      '0x51182c9B66F5Cb2394511006851aE9b1Ea7f1B5D',
+      '0x675eD17F58b15CD2C31F6d9bfb0b4DfcCA264eC1',
+      '0x6bAfFEE3c8B59E5bA19c26Cd409B2a232abb57Cb',
+      '0x36a2E8FF08f801caB399eab2fEe9E6A8C49A9C2A',
+      '0x3EC6676fa4D07C1f31d088ae1DE96240eC56D1D9',
+      '0x893e1c16fE47DF676Fd344d44c074096675B6aF6',
+      '0x3eEC4A51cbB2De4e8Cc6c9eE859Ad16E8a8693FC',
+      '0x9772Ef6AbC2Dfd879ebd88aeAA9Cf1e69a16fCF4',
+      '0x18991d6F877eF0c0920BFF9B14D994D80d2E7B0c',
     ],
     requiredOracles: 6,
+    oraclesConfigIpfsHash: 'QmZHT64Aauy8quU3nvsUoRp7cSmQQCmPD1fHhPuP3JMTHL',
   },
   [Networks.gnosis]: {
     url: process.env.GNOSIS_RPC_URL || '',
@@ -30,6 +31,7 @@ export const NETWORKS: {
     validatorsRegistry: '0x0B98057eA310F4d31F2a452B414647007d1645d9',
     oracles: [], // TODO: update with oracles' addresses
     requiredOracles: 6,
+    oraclesConfigIpfsHash: '',
   },
   [Networks.mainnet]: {
     url: process.env.MAINNET_RPC_URL || '',
@@ -38,5 +40,6 @@ export const NETWORKS: {
     validatorsRegistry: '0x00000000219ab540356cBB839Cbe05303d7705Fa',
     oracles: [], // TODO: update with oracles' addresses
     requiredOracles: 6,
+    oraclesConfigIpfsHash: '',
   },
 }
