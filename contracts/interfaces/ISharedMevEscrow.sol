5be351c927243e5e5979d8de0332aa43aa6e10b3
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-03-17 20:59:34 +0200

    Add SharedMevEscrow contract

---
+++ contracts/interfaces/ISharedMevEscrow.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.8.19;
+
+/**
+ * @title ISharedMevEscrow
+ * @author StakeWise
+ * @notice Defines the interface for the SharedMevEscrow contract
+ */
+interface ISharedMevEscrow {
+  error WithdrawalFailed();
+
+  /**
+   * @notice Event emitted on received MEV
+   * @param amount The amount of MEV received
+   */
+  event MevReceived(uint256 amount);
+
+  /**
+   * @notice Withdraws MEV accumulated in the escrow. Can be called only by the Vault.
+   * @dev IMPORTANT: because control is transferred to the Vault, care must be
+   *    taken to not create reentrancy vulnerabilities. The Vault must follow the checks-effects-interactions pattern:
+   *    https://docs.soliditylang.org/en/v0.8.19/security-considerations.html#use-the-checks-effects-interactions-pattern
+   * @param amount The amount of assets to withdraw
+   */
+  function withdraw(uint256 amount) external;
+}
---
+++ contracts/vaults/ethereum/mev/SharedMevEscrow.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.8.19;
+
+import {ISharedMevEscrow} from '../../../interfaces/ISharedMevEscrow.sol';
+import {IVaultsRegistry} from '../../../interfaces/IVaultsRegistry.sol';
+
+/**
+ * @title SharedMevEscrow
+ * @author StakeWise
+ * @notice Accumulates received MEV. The rewards are shared by multiple Vaults.
+ */
+contract SharedMevEscrow is ISharedMevEscrow {
+  IVaultsRegistry private immutable _vaultsRegistry;
+
+  /// @dev Constructor
+  constructor(address vaultsRegistry) {
+    _vaultsRegistry = IVaultsRegistry(vaultsRegistry);
+  }
+
+  /// @inheritdoc ISharedMevEscrow
+  function withdraw(uint256 amount) external override {
+    if (!_vaultsRegistry.vaults(msg.sender)) revert WithdrawalFailed();
+
+    (bool success, ) = payable(msg.sender).call{value: amount}('');
+    if (!success) revert WithdrawalFailed();
+  }
+
+  /**
+   * @dev Function for receiving MEV
+   */
+  receive() external payable {
+    emit MevReceived(msg.value);
+  }
+}
