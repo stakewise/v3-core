30a7114d5e49abcae3acd4e2ca8d167de781ec37
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-03-31 22:31:01 +0300

    Rename amount to assets in mev escrows (#45)

--- contracts/interfaces/ISharedMevEscrow.sol
+++ contracts/interfaces/ISharedMevEscrow.sol
@@ -12,23 +12,23 @@ interface ISharedMevEscrow {
 
   /**
    * @notice Event emitted on received MEV
-   * @param amount The amount of MEV received
+   * @param assets The amount of MEV assets received
    */
-  event MevReceived(uint256 amount);
+  event MevReceived(uint256 assets);
 
   /**
    * @notice Event emitted on harvest
    * @param caller The function caller
-   * @param amount The amount of assets withdrawn
+   * @param assets The amount of assets withdrawn
    */
-  event Harvested(address indexed caller, uint256 amount);
+  event Harvested(address indexed caller, uint256 assets);
 
   /**
    * @notice Withdraws MEV accumulated in the escrow. Can be called only by the Vault.
    * @dev IMPORTANT: because control is transferred to the Vault, care must be
    *    taken to not create reentrancy vulnerabilities. The Vault must follow the checks-effects-interactions pattern:
    *    https://docs.soliditylang.org/en/v0.8.19/security-considerations.html#use-the-checks-effects-interactions-pattern
-   * @param amount The amount of assets to withdraw
+   * @param assets The amount of assets to withdraw
    */
-  function harvest(uint256 amount) external;
+  function harvest(uint256 assets) external;
 }
