e74e088723ac31ec8eb3d3666f78d06b8c90e037
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-06-03 16:05:24 +0400

    Revert IKeeper interface

---
+++ contracts/interfaces/IKeeper.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.8.20;
+
+import {IKeeperValidators} from './IKeeperValidators.sol';
+import {IKeeperRewards} from './IKeeperRewards.sol';
+
+/**
+ * @title IKeeper
+ * @author StakeWise
+ * @notice Defines the interface for the Keeper contract
+ */
+interface IKeeper is IKeeperRewards, IKeeperValidators {
+
+}
