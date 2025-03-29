b1964f9480da589399fcec50caabea1f817e1301
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2025-03-29 16:16:55 +0200

    Add VaultToken tests

--- contracts/mocks/EthPrivVaultV6Mock.sol
+++ contracts/mocks/EthPrivVaultV6Mock.sol
@@ -5,13 +5,13 @@ pragma solidity ^0.8.22;
 import {IEthVault} from '../interfaces/IEthVault.sol';
 import {EthPrivVault} from '../vaults/ethereum/EthPrivVault.sol';
 
-contract EthPrivVaultV4Mock is EthPrivVault {
+contract EthPrivVaultV6Mock is EthPrivVault {
   uint128 public newVar;
 
   /// @custom:oz-upgrades-unsafe-allow constructor
   constructor(IEthVault.EthVaultConstructorArgs memory args) EthPrivVault(args) {}
 
-  function initialize(bytes calldata data) external payable virtual override reinitializer(4) {
+  function initialize(bytes calldata data) external payable virtual override reinitializer(6) {
     (newVar) = abi.decode(data, (uint128));
   }
 
@@ -20,6 +20,6 @@ contract EthPrivVaultV4Mock is EthPrivVault {
   }
 
   function version() public pure virtual override returns (uint8) {
-    return 4;
+    return 6;
   }
 }
--- contracts/mocks/EthVaultV6Mock.sol
+++ contracts/mocks/EthVaultV6Mock.sol
@@ -2,14 +2,21 @@
 
 pragma solidity ^0.8.22;
 
-import {IEthVault} from '../interfaces/IEthVault.sol';
-import {EthVaultV5Mock} from './EthVaultV5Mock.sol';
+import {EthVault, IEthVault} from '../vaults/ethereum/EthVault.sol';
 
-contract EthVaultV6Mock is EthVaultV5Mock {
+contract EthVaultV6Mock is EthVault {
+  uint128 public newVar;
+
   /// @custom:oz-upgrades-unsafe-allow constructor
-  constructor(IEthVault.EthVaultConstructorArgs memory args) EthVaultV5Mock(args) {}
+  constructor(IEthVault.EthVaultConstructorArgs memory args) EthVault(args) {}
 
-  function initialize(bytes calldata data) external payable override reinitializer(6) {}
+  function initialize(bytes calldata data) external payable virtual override reinitializer(6) {
+    (newVar) = abi.decode(data, (uint128));
+  }
+
+  function somethingNew() external pure returns (bool) {
+    return true;
+  }
 
   function version() public pure virtual override returns (uint8) {
     return 6;
