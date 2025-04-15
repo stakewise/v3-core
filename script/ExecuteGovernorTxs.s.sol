cdf8143e96c6417d1984e1c6c93420b08ad1ce71
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2025-04-15 13:13:55 +0300

    Update execute governor txs script

--- script/ExecuteGovernorTxs.s.sol
+++ script/ExecuteGovernorTxs.s.sol
@@ -4,50 +4,49 @@ pragma solidity ^0.8.22;
 
 import {console} from "forge-std/console.sol";
 import {stdJson} from "forge-std/StdJson.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {Network} from "./Network.sol";
 
 contract ExecuteGovernorTxs is Network {
     using stdJson for string;
 
+    struct Transaction {
+        bytes data;
+        string method;
+    }
+
     function run() public {
         // Read and parse the JSON file
         string memory jsonString = vm.readFile(getGovernorTxsFilePath());
+        console.log("JSON file loaded successfully");
 
-        // Get the number of transactions
-        uint256 txCount = jsonString.readUint(".transactions.length");
+        // Parse and extract transactions manually
+        bytes memory transactionsRaw = jsonString.parseRaw(".transactions");
+        Transaction[] memory transactions = abi.decode(transactionsRaw, (Transaction[]));
+
+        uint256 txCount = transactions.length;
+        console.log("Found %d transactions to execute", txCount);
 
         // Start broadcast with private key from environment variable
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         address sender = vm.addr(privateKey);
         console.log("Executing transactions from address: %s", sender);
 
-        vm.startBroadcast(privateKey);
+        Deployment memory deployment = getDeploymentData();
 
-        console.log("Found %d transactions to execute", txCount);
+        vm.startBroadcast(privateKey);
 
         // Execute each transaction
         for (uint256 i = 0; i < txCount; i++) {
-            string memory txPath = string.concat(".transactions[", vm.toString(i), "]");
+            Transaction memory transaction = transactions[i];
 
-            // Extract transaction data
-            bytes memory data = jsonString.readBytes(string.concat(txPath, ".data"));
-            address to = jsonString.readAddress(string.concat(txPath, ".to"));
-            string memory method = jsonString.readString(string.concat(txPath, ".method"));
-
-            console.log("Executing tx %d: %s to %s", i, method, to);
+            console.log("Executing tx %d: %s", i, transaction.method);
 
             // Execute the transaction
-            (bool success,) = to.call(data);
-
-            if (success) {
-                console.log("Transaction succeeded");
-            } else {
-                console.log("Transaction failed");
-                revert("Transaction execution failed");
-            }
+            Address.functionCall(deployment.vaultsRegistry, transaction.data);
         }
 
         vm.stopBroadcast();
-        console.log("All transactions executed successfully");
+        console.log("All transactions processed");
     }
 }
